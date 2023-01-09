035cc2ee3603357278500df02cfa9947857cafe2
jayanags <98667513+jayanags@users.noreply.github.com>
2023-01-08 16:15:08 -0800

    fixed problems in my_plot.py

--- my_plot.py
+++ my_plot.py
@@ -1,15 +1,16 @@
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as pyplot
 import os
 from astropy.io import fits
 import glob
 data = fits.getdata('../M100.mom0.fits')
-xlen = np.shape(data)[0]
-col1 = data[xlen, :]
-col2 = data[200, :]
-col3 = data[800, :]
+print(np.shape(data))
+xlen,ylen = np.shape(data)[0], np.shape(data)[1]
+col1 = data[xlen-1, :]
+col2 = data[ylen-1, :]
+col3 = data[ylen-1, :]
 pyplot.figure()
-pyplot.plot(np.arange(1024), col1)
+pyplot.plot(np.arange(67), col1)
 mean = np.mean(col1)
 pyplot.axhline(mean)
 pyplot.legend(['Column 20', 'Mean'])
@@ -17,7 +18,7 @@ pyplot.title('Plot of Column 20 Values and Mean')
 pyplot.xlabel('Pixels')
 pyplot.ylabel('Intensity')
 pyplot.figure()
-pyplot.plot(np.arange(1024), col2)
+pyplot.plot(np.arange(67), col2)
 mean = np.mean(col2)
 pyplot.axhline(mean)
 pyplot.legend(['Column 200', 'Mean'])
@@ -25,10 +26,11 @@ pyplot.title('Plot of Column 200 Values and Mean')
 pyplot.xlabel('Pixels')
 pyplot.ylabel('Intensity')
 pyplot.figure()
-pyplot.plot(np.arange(1024), col3)
+pyplot.plot(np.arange(67), col3)
 mean = np.mean(col3)
 pyplot.axhline(mean)
 pyplot.legend(['Column 800', 'Mean'])
 pyplot.title('Plot of Column 800 Values and Mean')
 pyplot.xlabel('Pixels')
 pyplot.ylabel('Intensity')
+pyplot.show()
